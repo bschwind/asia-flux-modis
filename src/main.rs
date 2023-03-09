a56a8f1f4907b9a6b22cb6aec0f2b8949560c76a
Dan Henri <djosephhenri@gmail.com>
2023-03-09 15:07:07 +0900

    refactored get_modis_data function

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ const LINES: u64 = 43200;
 const PIXEL_SIZE: f64 = 180.0 / LINES as f64;
 
 fn get_modis_data(record: StringRecord) -> io::Result<()> {
+    let mut array_500m: [[u8; 7]; 7] = Default::default();
     let mut modis_binary: ModisBin500m = Default::default();
 
     let tower_entry_data = TowerEntryData {
@@ -57,26 +58,43 @@ fn get_modis_data(record: StringRecord) -> io::Result<()> {
         println!("{file_string}");
         let seek_line: u64 = LINES - ((tower_entry_data.lat + 90.0) / PIXEL_SIZE) as u64 - 1;
         let seek_pixel: u64 = ((tower_entry_data.lon + 180.0) / PIXEL_SIZE) as u64;
-        let seek_point: u64 = (seek_line * PIXELS - 1) + seek_pixel;
+        let seek_point: u64 = (seek_line * PIXELS - 1) + seek_pixel - 3;
         let mut file = BufReader::new(File::open(file_string)?);
 
-        file.seek(SeekFrom::Start(seek_point - 3 - (PIXELS * 3)))?;
+        for (i, array) in array_500m.iter_mut().enumerate() {
+            let line_multiplier: i64 = i as i64 - 3;
+            println!(
+                "iter: {}",
+                (seek_point as i64 + (PIXELS as i64 * line_multiplier)) as u64
+            );
+            file.seek(SeekFrom::Start(
+                (seek_point as i64 + (PIXELS as i64 * line_multiplier)) as u64,
+            ))?;
+            file.read_exact(array)?;
+        }
+        file.seek(SeekFrom::Start(seek_point - (PIXELS * 3)))?;
+        println!("non iter: {}", seek_point - (PIXELS * 3));
         file.read_exact(&mut modis_binary.first)?;
-        file.seek(SeekFrom::Start(seek_point - 3 - (PIXELS * 2)))?;
+        // file.read_exact(&mut array_500m[0])?;
+        file.seek(SeekFrom::Start(seek_point - (PIXELS * 2)))?;
         file.read_exact(&mut modis_binary.second)?;
-        file.seek(SeekFrom::Start(seek_point - 3 - PIXELS))?;
+        file.seek(SeekFrom::Start(seek_point - PIXELS))?;
         file.read_exact(&mut modis_binary.third)?;
-        file.seek(SeekFrom::Start(seek_point - 3))?;
+        file.seek(SeekFrom::Start(seek_point))?;
         file.read_exact(&mut modis_binary.fourth)?;
+        file.seek(SeekFrom::Start(seek_point + (PIXELS)))?;
+        file.read_exact(&mut modis_binary.fifth)?;
+        file.seek(SeekFrom::Start(seek_point + (PIXELS * 2)))?;
+        file.read_exact(&mut modis_binary.sixth)?;
+        file.seek(SeekFrom::Start(seek_point + (PIXELS * 3)))?;
+        file.read_exact(&mut modis_binary.seventh)?;
 
         let mut value: [u8; 1] = Default::default();
         file.seek(SeekFrom::Start(seek_point))?;
         file.read_exact(&mut value)?;
-        println!("seek_line: {seek_line}");
-        println!("seek_pixel: {seek_pixel}");
-        println!("exact value: {value:?}");
         println!("tower data: {:?}", tower_entry_data);
         println!("value: {:?}", modis_binary);
+        println!("array: {array_500m:?}")
     }
 
     Ok(())
